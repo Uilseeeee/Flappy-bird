2850f608040780de51bd846fa95e1e3481493b05
Uilsee <uilseeu437@gmail.com>
2024-12-16 12:46:45 +0800

    just started

---
+++ ls.js
@@ -0,0 +1,15 @@
+var block = document.getElementById("block");
+
+var hole = document.getElementById("hole");
+
+var character = document.getElementById("character");
+
+hole.addEventListener('animationiteration', () => {
+  var random = -((Math.random() * 300) + 150);
+  hole.style.top = random + "px";
+});
+setInterval(function () {
+  var characterTop =
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    character.style.top = (characterTop+3)+"px";
+}, 10);
