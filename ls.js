6d70188bdac6c12f52c9e5a9d8c39b67b6de0048
Uilsee <uilseeu437@gmail.com>
2024-12-17 12:46:18 +0800

    fixing jump

--- ls.js
+++ ls.js
@@ -1,15 +1,37 @@
-var block = document.getElementById("block");
+let block = document.getElementById("block");
 
-var hole = document.getElementById("hole");
+let hole = document.getElementById("hole");
 
-var character = document.getElementById("character");
+let character = document.getElementById("character");
+
+let jumping = 0;
 
 hole.addEventListener('animationiteration', () => {
-  var random = -((Math.random() * 300) + 150);
+  let random = -((Math.random() * 300) + 150);
   hole.style.top = random + "px";
 });
 setInterval(function () {
-  var characterTop =
+  let characterTop =
     parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    character.style.top = (characterTop+3)+"px";
+  if (jumping == 0) {
+
+    character.style.top = (characterTop + 3) + "px";
+  }
 }, 10);
+function jump() {
+  jumping = 1;
+  let jumpCount = 0;
+  let jumpInterval = setInterval (function () {
+    let characterTop =
+      parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if ((character > 6) && (counter < 100)) {
+      character.style.top = (characterTop - 5) + "px";
+    }
+    if (jumpCount > 20) {
+      clearInterval(jumpInterval);
+      jumping = 0;
+      jumpCount = 0;
+    }
+    jumpCount++;
+  },10);
+}
